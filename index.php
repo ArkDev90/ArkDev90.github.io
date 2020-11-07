abf44c3d85f5c131d9614ff0bec91f4985d27233
Ark-DOSTNCR <larklatoja.dost.ncr@gmail.com>
2020-11-07 13:49:04 +0800

    first init

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/logo.png" />
+    <title>Development</title>
+
+      <style>
+        .center {
+          display: block;
+          margin-left: auto;
+          margin-right: auto;
+          width: 50%;
+        }
+
+      </style>
+
+    </head>
+
+    <body  style="">
+        <img src="assets/images/logo.jpg" class="center" alt="logo" width="auto" height="auto">
+    </body>
+</html>
